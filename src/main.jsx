ae8389ecb849f9d3074d4e24887846963be9e72a
yrkim2605-wq <yrkim2605@gmail.com>
2026-08-27 10:48:19 +0900

    initial setup

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import './index.css'
+import App from './App.jsx'
+import theme from './theme.js'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+)
